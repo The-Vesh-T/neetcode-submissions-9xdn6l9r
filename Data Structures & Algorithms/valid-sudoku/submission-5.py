b6a390309b2c210609169355f9ddeae01afd0dec
Devesh <100452856+The-Vesh-T@users.noreply.github.com>
2026-08-07 11:38:48 -0700

    Add: valid-sudoku - submission-5

---
+++ Data Structures & Algorithms/valid-sudoku/submission-5.py
@@ -0,0 +1,40 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        #check row, check col, check squar
+        for row in range(9):
+            seen = set()
+            for i in range(9):
+                if board[row][i] == '.':
+                    continue
+                if board[row][i] in seen:
+                    return False
+                seen.add(board[row][i])
+        
+        for col in range(9):
+            seen = set()
+            for i in range(9):
+                if board[i][col] == '.':
+                    continue
+                if board[i][col] in seen:
+                    return False
+                seen.add(board[i][col])
+        
+        start_pos =[(0,0), (0,3), (0,6),
+                    (3,0), (3,3), (3,6),
+                    (6,0), (6,3), (6,6)]
+        #with these start positions jus tierate through in a tighter loop
+        for r, c in start_pos:
+            seen = set()
+            for row in range(r, r+3):
+                for col in range(c, c+3):
+                    item = board[row][col]
+                    if item == '.':
+                        continue
+                    if item in seen:
+                        return False
+                    seen.add(item)
+        return True
+
+
+
+
